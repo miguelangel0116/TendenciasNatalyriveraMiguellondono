795338c60f3ec8f3706800347507e9d711203da7
miguelangel0116 <miguel.0116@outlook.com>
2024-03-02 07:58:17 -0500

    actualizar

--- Hospital/main.py
+++ Hospital/main.py
@@ -11,11 +11,16 @@ def MenuAdminRH(Hos,user):
     option=input("1. Crear nuevo usuario\n 2.eliminar usuario\n 3. gestionar permisos de usuario\n 4. Actualizacion de datos personales\n  5.Listar todos los usuarios\n 6.Cerrar sesion")
     
 def MenuDoctor(Hos,user):
-    option=input("1. ver historial clinico")
+    option=input("1. ver historial clinico\n 2.Crear nuevo registro médico\n 3.Actualizar registro médico existente\n 4. Prescribir tratamiento\n 5. Ordenar pruebas médicas\n 6. Cerrar sesión")
+
+
 def MenuSeller(Hos,user):
     pass
 def MenupersonAdmin(Hos,user):
     option=input("1.Registrar paciente\n 2. Programar cita para paciente \n 3.Registrar informacion de facturacion \n 4. Ver historial y registros de un paciente \n 5. Listar todos los pacientes \n 6.Cerrar sesion")
+
+def MenuNurse(Hos,user):
+    option=input("1. Registrar datos vitales de un paciente\n 2. Registrar medicamentos administrados\n 3. Registrar procedimientos realizados\n 4. Registrar pruebas médicas\n 5. Ver observaciones y detalles de la atención del paciente\n 6. Cerrar sesión")
 
 def LoginRouter(Hos,user):
     print(user.rol)
@@ -27,6 +32,8 @@ def LoginRouter(Hos,user):
         MenuSeller(Hos,user)
     elif user.rol=="PersonAdmin":
         MenupersonAdmin(Hos,user)
+    elif user.rol=="nurse":
+        MenuNurse(Hos,user)
     else:
         print("El usuario no tiene un rol valido")
     
